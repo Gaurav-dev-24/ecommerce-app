f6b72c1dd986d5075840ff78c8f50d389ea7c57c
Gaurav Jangid <gauravjangid@Gauravs-MacBook-Pro.local>
2025-07-01 18:39:13 +0530

    All modifications: frontend, backend, admin updated and ready for deployment

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import '../src/index.css'
-import App from './App.jsx'
+import App from '../src/App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
